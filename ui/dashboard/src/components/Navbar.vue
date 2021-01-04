c141b9801de8a87e7bc228145e70069b65072e1e
Kushan Shamika <shamikakushan@gmail.com>
2021-01-04 22:27:30 +0530

    add app bar

---
+++ ui/dashboard/src/components/Navbar.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <v-app-bar
+        color="deep-purple accent-4"
+        dense
+        dark
+    >
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+      <v-toolbar-title>LQ Highlights</v-toolbar-title>
+
+    </v-app-bar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Navbar"
+}
+</script>
